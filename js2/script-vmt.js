f754bcd60991ad596747ddd2d47e9cae2df2a9b8
Jialin Li <32602256+JialinLiOSU@users.noreply.github.com>
2019-05-24 09:56:33 -0400

    modify bar chart

--- js2/script-vmt.js
+++ js2/script-vmt.js
@@ -8,7 +8,7 @@ var cogo_layer = null;
 
 
 
-var cogo_margin = { top: 20, right: 5, bottom: 90, left: 30 }; // bottom incl. legend and text
+var cogo_margin = { top: 20, right: 35, bottom: 90, left: 35 }; // bottom incl. legend and text
 var cogo_color = d3.scale.ordinal()
     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
@@ -31,10 +31,8 @@ cogo_height = $(cogo_id).height() - cogo_margin.top - cogo_margin.bottom;
 var data = [9524.4, 0, 9231.9, 9514.7, 9482.8];
 
 var width = cogo_width,
-    Height = cogo_height,
-    barHeight = Height / 5 - 20;
-
-
+    Height = cogo_height;
+var barWidth = width / data.length;
 
 var y = d3.scale.linear()
     .domain([0, d3.max(data)])
@@ -42,31 +40,7 @@ var y = d3.scale.linear()
 
 var x = d3.scale.ordinal()
     .domain(["2013", "2014", "2015", "2016", "2017"])
-    .rangeRoundBands([0, $(cogo_id).width()], 1);
-
-var chart = d3.select(cogo_id).append("svg")
-    .attr("width", $(cogo_id).width())
-    .attr("height", $(cogo_id).height());
-
-var barWidth = width / data.length;
-
-var bar = chart.selectAll("g")
-    .data(data)
-    .enter().append("g")
-    .attr("transform", function (d, i) { return "translate(" + i * barWidth + ",0)"; });
-
-bar.append("rect")
-    .attr("y", function (d) { return y(d); })
-    .attr("height", function (d) { return Height - y(d); })
-    .attr("width", barWidth - 1)
-    .style("fill", "steelblue");
-
-bar.append("text")
-    .attr("x", barWidth / 2)
-    .attr("y", function (d) { return y(d) + 3; })
-    .attr("dy", ".75em")
-    .text(function (d) { return d; });
-
+    .rangeRoundBands([barWidth/2, width+barWidth/2], 0);
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -74,10 +48,40 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
-bar.append("g")
+
+var chart = d3.select(cogo_id).append("svg")
+    .attr("width", $(cogo_id).width())
+    .attr("height", $(cogo_id).height());
+chart.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + Height + ")")
     .call(xAxis);
-bar.append("g")
+chart.append("g")
     .attr("class", "y axis")
+    .attr("transform", "translate("+cogo_margin.left+",0)")
     .call(yAxis);
+
+var bar = chart.selectAll(".bar")
+    .data(data)
+    .enter().append("g")
+    .attr("transform", function (d, i) { return "translate(" + i * barWidth + ",0)"; });
+
+bar.append("rect")
+    .attr("x", cogo_margin.left+5)
+    .attr("y", function (d) { return y(d); })
+    // .attr("y", 0)
+    .attr("height", function (d) { return Height - y(d); })
+    .attr("width", barWidth-1 )
+    .style("fill", "steelblue");
+
+bar.append("text")
+    .attr("x", 90 )
+    .attr("y", function (d) { return y(d) -15; })
+    .attr("dy", ".75em")
+    .text(function (d) { return d; })
+    .style("fill", "black");;
+
+// bar.append("g")
+//     .attr("class", "x axis")
+//     .attr("transform", "translate(0," + Height + ")")
+//     .call(xAxis);
